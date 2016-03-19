35aef13b1e5130eade8ddbfcce971a66e155ae19
Ryan Tuck <ryntck@gmail.com>
2016-03-19 11:12:24 -0400

    add images and quantities to ingredients

--- wontons.js
+++ wontons.js
@@ -4,18 +4,22 @@ var width = 1200,
     height = 800;
 
 // rect dimensions
-var w_rect = 160,
-    h_rect = 60;
+var w_step = 160,
+    h_step = 60;
+
+// rect dimensions
+var w_ingredient = 200,
+    h_ingredient = 60;
 
 // spacing between nodes
-var x_spacing = 200,
-    y_spacing = 80;
+var x_spacing = 40,
+    y_spacing = 40;
 
 // margin around content
 var margin = 100;
 
 // set color palette
-var c10 = d3.scale.category10();
+// var c10 = d3.scale.category10();
 
 // create main svg body
 var svg = d3.select("body").append("svg")
@@ -32,12 +36,12 @@ function get_coordinates(d, i) {
         delX = 0;
     }
     else if (d.type == 'step') {
-        delX = x_spacing * (1 + d.level);
+        delX = w_ingredient + x_spacing * (d.level+1) + w_step * (d.level);
     }
 
     return {
         'x': base + delX,
-        'y': base + y_spacing*i
+        'y': base + (h_ingredient + y_spacing)*i
     };
 }
 
@@ -81,16 +85,33 @@ d3.json('wonton-soup.json', function(error, graph) {
     // define rectangles
     ingredient_nodes.append("rect")
         .attr("class", "node")
-        .attr("width", w_rect)
-        .attr("height", h_rect)
-        .attr("fill", function(d,i){ return c10(i); });
+        .attr("width", w_ingredient)
+        .attr("height", h_ingredient)
+        .attr("fill", function(d,i){ return "white"; });
 
-    // define text
+    // text - title
     ingredient_nodes.append("text")
         .attr("dy", ".35em")
         .attr("y", "10")
         .text(function(d) { return d.title; })
-        .call(wrap, w_rect);
+        .attr('transform', 'translate(65,0)')
+        .call(wrap, w_step);
+
+    // quantities text
+    ingredient_nodes.append("text")
+        .attr("dy", ".35em")
+        .attr("y", "40")
+        .text(function(d) { return d.quantity; })
+        .attr('transform', 'translate(65,0)')
+        .call(wrap, w_step);
+
+    // append images
+    ingredient_nodes.append('svg:image')
+        .attr('x', 0)
+        .attr('y', 0)
+        .attr('width', 60)
+        .attr('height', 60)
+        .attr('xlink:href', function(d) { return 'img/' + d.img;});
 
     // ------------------------------------------------
     // position and populate step nodes
@@ -109,21 +130,32 @@ d3.json('wonton-soup.json', function(error, graph) {
     // define rectanges
     step_nodes.append("rect")
         .attr("class", "asdf")
-        .attr("width", w_rect)
-        .attr("height", h_rect)
-        .attr("fill", function(d,i){ return c10(i); });
+        .attr("width", w_step)
+        .attr("height", h_step)
+        .attr("fill", function(d,i){ return "white"; });
 
     // define text
     step_nodes.append("text")
             .attr("dy", ".35em")
             .attr("y", "10")
         .text(function(d) {return d.title;})
-        .call(wrap, w_rect);
+        .call(wrap, w_step);
 
 
     // ------------------------------------------------
     // populate links
     // ------------------------------------------------
+
+    var types = {
+        'ingredient': {
+            'list': ingredients,
+            'width': w_ingredient
+        },
+        'step': {
+            'list': steps,
+            'width': w_step
+        }
+    };
 
     var links  = svg.selectAll("link")
         .data(graph.links)
@@ -141,22 +173,18 @@ d3.json('wonton-soup.json', function(error, graph) {
 
             // determine index of starting node based on its type
             var i = 0;
-            var lists = {
-                'ingredient': ingredients,
-                'step': steps
-            };
-            i = lists[sourceNode.type].map(function(e) {
+            i = types[sourceNode.type].list.map(function(e) {
                 return e.id;
             }).indexOf(l.from_id);
 
 
             // define y coord based on source node and index
             d3.select(this).attr("y1",function(){
-                return get_coordinates(sourceNode, i).y + h_rect/2;
+                return get_coordinates(sourceNode, i).y + h_step/2;
             });
 
             // define x coord from source node and index
-            return get_coordinates(sourceNode, i).x + w_rect;
+            return get_coordinates(sourceNode, i).x + types[sourceNode.type].width;
         })
         .attr("x2",function(l){
             // get target node
@@ -168,18 +196,14 @@ d3.json('wonton-soup.json', function(error, graph) {
 
             // determine index of target node based on its type
             var i = 0;
-            var lists = {
-                'ingredient': ingredients,
-                'step': steps
-            };
-            i = lists[targetNode.type].map(function(e) {
+            i = types[targetNode.type].list.map(function(e) {
                 return e.id;
             }).indexOf(l.to_id);
 
 
             // define y coord based on target node and index
             d3.select(this).attr("y2",function(){
-                return get_coordinates(targetNode, i).y + h_rect/2;
+                return get_coordinates(targetNode, i).y + h_step/2;
             });
 
             // define x coord from target node and index
